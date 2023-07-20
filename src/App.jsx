7dc814327d4bca9ed51d6931d417023225cfde59
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-20 09:38:39 -0300

    instalo bootstrap y empiezo la estructura de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
   return (
     <>
-      <h1>Paleta de colores</h1>
+      <header>
+        <h2 className="text-center">
+          Paletas de colores
+          <img
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Activities/Artist%20Palette.png"
+            alt="Artist Palette"
+            className="paletteColor"
+          />
+        </h2>
+      </header>
+      <main>
+        <h2>Hola Main</h2>
+      </main>
+      <footer>
+        <h5 className="text-center">&copy; Todos los derechos reservados.</h5>
+      </footer>
     </>
   );
 };
