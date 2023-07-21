2f2be57f05489dffd85321907828e7e4db9e7db7
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-21 00:50:33 -0300

    Añadiendo la funcion para borrar colores de la lista

--- src/components/ListColors.jsx
+++ src/components/ListColors.jsx
@@ -1,11 +1,11 @@
 import ItemColor from "./ItemColor";
 
-const ListColors = ({ listColors }) => {
+const ListColors = ({ listColors, deletColor }) => {
   return (
     <section className="container my-4">
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
         {listColors.map((color) => (
-          <ItemColor key={color} color={color} />
+          <ItemColor key={color} color={color} deletColor={deletColor} />
         ))}
       </div>
     </section>
